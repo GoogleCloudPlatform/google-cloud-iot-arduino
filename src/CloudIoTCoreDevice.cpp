ba7f418e36402355c139134116adccd5de61b8b0
Gus Class <gguuss@gmail.com>
2018-11-18 01:58:19 -0800

    Removes extra assignment

--- src/CloudIoTCoreDevice.cpp
+++ src/CloudIoTCoreDevice.cpp
@@ -95,7 +95,6 @@ String CloudIoTCoreDevice::getSetStatePath() {
 }
 
 void CloudIoTCoreDevice::fillPrivateKey() {
-  priv_key[8] = 0;
   for (int i = 7; i >= 0; i--) {
     priv_key[i] = 0;
     for (int byte_num = 0; byte_num < 4; byte_num++) {
