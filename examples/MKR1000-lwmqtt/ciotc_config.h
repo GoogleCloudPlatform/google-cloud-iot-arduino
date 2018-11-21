0411929e1b3c65a01b275311d78e8c1fe0eab5cf
Gus Class <gguuss@gmail.com>
2018-11-21 15:59:10 -0800

    Adds MKR1000 lwMQTT example

---
+++ examples/MKR1000-lwmqtt/ciotc_config.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// This file contains your configuration used to connect to Cloud IoT Core
+
+// Wifi newtork details.
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+// Cloud iot details.
+const char* project_id = "YOUR-PROJECT-ID";
+const char* location = "us-central1";
+const char* registry_id = "YOUR-REGISTRY-ID";
+const char* device_id = "YOUR-DEVICE-ID";
+
+// To get the private key run (where private-key.pem is the ec private key
+// used to create the certificate uploaded to google cloud iot):
+// openssl ec -in <private-key.pem> -noout -text
+// and copy priv: part.
+// The key length should be exactly the same as the key length bellow (32 pairs
+// of hex digits). If it's bigger and it starts with "00:" delete the "00:". If
+// it's smaller add "00:" to the start. If it's too big or too small something
+// is probably wrong with your key.
+const char* private_key_str =
+    "5a:2e:06:b5:c1:f2:9c:b3:77:b2:89:f5:29:29:93:"
+    "07:fd:ed:22:0d:03:2b:a6:b1:b6:04:0b:d5:9b:49:"
+    "7d:ca";
+
+
+// TODO(you): Install root certificate to verify tls connection as described
+// in https://www.hackster.io/arichetta/add-ssl-certificates-to-mkr1000-93c89d
