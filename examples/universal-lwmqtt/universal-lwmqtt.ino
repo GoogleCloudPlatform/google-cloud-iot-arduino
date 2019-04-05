53c8e89e2764806418475843600a52b1e8eb2012
Gus Class <gguuss@gmail.com>
2019-04-05 12:23:51 -0700

    Single example that works in all supported architectures (#100)
    This approach to a unified MQTT example uses architecture detection to conditionally inject the architecture specific code.

---
+++ examples/universal-lwmqtt/universal-lwmqtt.ino
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// !!REPLACEME!!
+// The MQTT callback function for commands and configuration updates
+// Place your message handler code here.
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+
+#include "universal-mqtt.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  setupCloudIoT();
+}
+
+unsigned long lastMillis = 0;
+void loop() {
+  mqttClient->loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!mqttClient->connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    publishTelemetry(getDefaultSensor());
+  }
+}
